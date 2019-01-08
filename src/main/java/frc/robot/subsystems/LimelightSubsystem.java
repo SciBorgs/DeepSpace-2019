727622d879f3626554c641acd5e564b6fc5b5960
zevbo <minsky-pz@bxscience.edu>
2019-01-08 16:52:32 -0500

    fixed dumb bbbug

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -11,10 +11,10 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimelightSubsystem extends Subsystem{
 
-    public getCameraData(String variable){ // According to API, should get a given variable (x1,a2... etc.)
+    public double getCameraData(String variable){ // According to API, should get a given variable (x1,a2... etc.)
         NetworkTableInstance.getDefault().getTable("limelight").getEntry(variable).getDouble(0);
     }
-    public setCameraParams(String param, int setting){ // According to API, should set a given param (camMode, pipeline... etc.)
+    public double setCameraParams(String param, int setting){ // According to API, should set a given param (camMode, pipeline... etc.)
         NetworkTableInstance.getDefault().getTable("limelight").getEntry(param).setNumber(setting);
     }
 
