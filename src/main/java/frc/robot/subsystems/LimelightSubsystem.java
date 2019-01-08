522377335ecb257a6d1fcbefef483688c66a5c0c
zevbo <minsky-pz@bxscience.edu>
2019-01-08 17:01:42 -0500

    now you get a table, and look up its data

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -7,12 +7,17 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimelightSubsystem extends Subsystem{
 
-    public double getCameraData(String variable){ // According to API, should get a given variable (x1,a2... etc.)
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry(variable).getDouble(0);
+    
+    public NetworkTable getCameraTable(String variable){ // According to API, should get a given variable (x1,a2... etc.)
+        return NetworkTableInstance.getDefault().getTable("limelight");
+    }
+    public double getTableData(NetworkTable table, String variable){
+        return table.getEntry(variable).getDouble(0);
     }
     public double setCameraParams(String param, int setting){ // According to API, should set a given param (camMode, pipeline... etc.)
         NetworkTableInstance.getDefault().getTable("limelight").getEntry(param).setNumber(setting);
