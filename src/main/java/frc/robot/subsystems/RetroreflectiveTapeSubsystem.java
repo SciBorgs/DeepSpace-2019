0071cbe5a7fc71ad184e71e228ad8378cda9b0df
8bitzawad <32941432+8bitzawad@users.noreply.github.com>
2019-01-08 16:31:18 -0500

    initial commit

---
+++ src/main/java/frc/robot/subsystems/RetroreflectiveTapeSubsystem.java
@@ -0,0 +1,22 @@
+package main.java.frc.robot.subsystems;
+
+import org.usfirst.frc.team1155.robot.Robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class RetroreflectiveTapeSubsystem extends Subsystem{
+
+   
+
+    @Override
+    protected void initDefaultCommand() {
+        // LITTERALLY DIE
+    }
+}
