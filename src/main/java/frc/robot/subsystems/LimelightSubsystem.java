a0454ac4c9b44522b040731d5020e4de39d69c28
zevbo <minsky-pz@bxscience.edu>
2019-01-08 16:21:00 -0500

    added functions, might work for getting data

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -7,12 +7,19 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimelightSubsytem extends Subsystem{
+
+    
+    public getCameraData(String variable){ // According to API, should get a given variable (x1,a2... etc.)
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry(variable).getDouble(0);
+    }
+    public setCameraParams(String param, int setting){ // According to API, should set a given param (camMode, pipeline... etc.)
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry(param).setNumber(setting);
+    }
 
     @Override
     protected void initDefaultCommand() {
