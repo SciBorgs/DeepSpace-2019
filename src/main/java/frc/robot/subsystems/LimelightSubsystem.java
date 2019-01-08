35d895f84a9556ba71deccca6c8bf36d2169356a
zevbo <minsky-pz@bxscience.edu>
2019-01-08 16:05:04 -0500

    empty limelight system added

---
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -0,0 +1,16 @@
+package main.java.frc.robot.subsystems;
+
+import org.usfirst.frc.team1155.robot.Robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class LimelightSubsytem extends Subsbytem{
+    
+    @Override
+    protected void initDefaultCommand() {
+        // LITTERALLY DIE
+    }
+}
