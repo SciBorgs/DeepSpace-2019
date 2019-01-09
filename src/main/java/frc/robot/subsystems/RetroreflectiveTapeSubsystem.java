6e4fcbe44ed7cded4d1b6144f08b7e403f797a3a
morokei <morokei-git@outlook.com>
2019-01-09 00:12:32 -0500

    Resolve syntax errors

--- src/main/java/frc/robot/subsystems/RetroreflectiveTapeSubsystem.java
+++ src/main/java/frc/robot/subsystems/RetroreflectiveTapeSubsystem.java
@@ -11,51 +11,62 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import java.util.Arrays;
-public class RetroreflectiveTapeSubsystem extends Subsystem{
 
-    public NetworkTable getTable(){return Robot.limelight.getCameraTable();}
-    public double get(NetworkTable table, String variable){return Robot.limelight.getTableDat(table,variable);}
+public class RetroreflectiveTapeSubsystem extends Subsystem {
 
-    public int[] center(){
+    public NetworkTable getTable() {
+        return Robot.limelight.getCameraTable();
+    }
+
+    public double get(NetworkTable table, String variable) {
+        return Robot.limelight.getTableDat(table, variable);
+    }
+
+    public double[] center() {
         NetworkTable table = getTable();
-        double[][] allvalues = {{get(table,"tx0"), get(table, "ty0")}, {get(table, "tx1"), get(table, "ty1")},{get(table, "tx2"), get(table, "ty2")}};
-        double[] xvalues = {allvalues[0][0], allvalues[0][0], allvalues[0][0]};
-        xvalues = Arrays.sort(xvalues);
-        
-        for (i = 0; i<3;i++){
-            if (allvalues[i][0] == xvalues[1]){
-                double angle = (get(table,"ts" + Integer.toString(i)));
+        double[][] allvalues = { { get(table, "tx0"), get(table, "ty0") }, { get(table, "tx1"), get(table, "ty1") },
+                { get(table, "tx2"), get(table, "ty2") } };
+        double[] xvalues = { allvalues[0][0], allvalues[0][0], allvalues[0][0] };
+        Arrays.sort(xvalues);
+
+        double angle = 0.0;
+        double[] finalavg;
+
+        for (int i = 0; i < 3; i++) {
+            if (allvalues[i][0] == xvalues[1]) {
+                angle = (get(table, "ts" + Integer.toString(i)));
             }
         }
-        
-        //left
-        if (angle > -45){
+
+        // left
+        if (angle > -45) {
             double avgx = (xvalues[1] + xvalues[0]) / 2.0;
             double avgy = 0;
-            for (i = 0; i<3;i++){
-                if ((allvalues[i][0] == xvalues[1]) || (allvalues[i][0] == xvalues[0])){
+            for (int i = 0; i < 3; i++) {
+                if ((allvalues[i][0] == xvalues[1]) || (allvalues[i][0] == xvalues[0])) {
                     avgy = avgy + allvalues[i][1];
                 }
             }
             avgy = avgy / 2.0;
-            double[][] finalavg = {avgx, avgy};
+            finalavg = new double[] { avgx, avgy };
             return finalavg;
         }
 
-        //right
-        else if (angle < -45){
+        // right
+        else if (angle < -45) {
             double avgx = (xvalues[1] + xvalues[2]) / 2.0;
             double avgy = 0;
-            for (i = 0; i<3;i++){
-                if ((allvalues[i][0] == xvalues[1]) || (allvalues[i][0] == xvalues[2])){
+            for (int i = 0; i < 3; i++) {
+                if ((allvalues[i][0] == xvalues[1]) || (allvalues[i][0] == xvalues[2])) {
                     avgy = avgy + allvalues[i][1];
                 }
             }
             avgy = avgy / 2.0;
-            double[][] finalavg = {avgx, avgy};
+            finalavg = new double[] { avgx, avgy };
             return finalavg;
         }
 
+        return finalavg;
     }
 
     @Override
