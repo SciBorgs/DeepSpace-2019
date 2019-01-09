fa76c8e4d39a177cd6e8e4e01b234fc8472f3c15
zevbo <minsky-pz@bxscience.edu>
2019-01-09 16:24:12 -0500

    added calclulations for centerX, centerY, distance and shift

--- src/main/java/frc/robot/subsystems/RetroreflectiveTapeSubsystem.java
+++ src/main/java/frc/robot/subsystems/RetroreflectiveTapeSubsystem.java
@@ -15,6 +15,9 @@ import java.util.Hasthable;
 
 public class RetroreflectiveTapeSubsystem extends Subsystem {
 
+    public final static double meterDegreeLength = 1; // Needs to be measured
+    public final static double meterArea = 40; // Needs to be measured
+
     public NetworkTable getTable() {
         return Robot.limelight.getCameraTable();
     }
@@ -23,38 +26,56 @@ public class RetroreflectiveTapeSubsystem extends Subsystem {
         return Robot.limelight.getTableDat(table, variable);
     }
 
-    // Below are helper functions for center()
+    // Below are helper functions for extractData()
     public boolean dataCompare(Hashtable<String,Double> pair1, Hashtable<String,Double> pair2){
-        return pair1.get("tx") > pair2.get("tx");
+        return pair1.get("x") > pair2.get("x");
     } 
     public double[] averagePos(Hashtable<String,Double> data1, Hashtable<String,Double> data2){
-        return new double[]{(data1.get("tx") + data2.get("tx"))/2,
-                            (data1.get("ty") + data2.get("ty"))/2};
+        return new double[]{(data1.get("x") + data2.get("x"))/2,
+                            (data1.get("y") + data2.get("y"))/2};
     }
     public boolean facingLeft(double skew){return skew > -45;}
     public Hashtable<String,Double> createData(NetworkTale t, double nth){
         // Takes a snapshot of data and the nth contour you want and converts it into a hashtable
         Hashtable<String, Double> end = new Hashtable<String, Double>();
         end.put("nth",nth);
-        end.put("tx",get(t,"tx" + nth));
-        end.put("ty",get(t,"ty" + nth));
-        end.put("ts",get(t,"ts" + nth));
+        end.put("x",get(t,"tx" + nth));
+        end.put("y",get(t,"ty" + nth));
+        end.put("s",get(t,"ts" + nth));
+        end.put("a",get(t,"ta" + nth));
         return end;
     }
-    // Above are helper functions for center
 
-    public double[] center() {
-        // Find the center of the two retroflective pieces of tape that are facing twoards each other!
-        NetworkTable t = getTable();
-        Hashtable<String,Double>[] values = new Hashtable<String, Double>[]{createData(t,0),createData(t,1),createData(t,2)};
+    public double[] center(Hashtable<String,Double>[] values){
         Arrays.sort(values,dataCompare);
-        boolean leftPair = facingLeft(values[1].get("ts"));
+        boolean leftPair = facingLeft(values[1].get("s"));
         if (leftPair)
             return averagePos(values[0],values[1]);
         else
             return averagePos(values[2],values[1]);
     }
-    
+
+    // Above are helper functions for extractData()
+
+    public Hashtable<String,Double> extractData(){
+        // Extracts data from the given data. Currently: Center position, distance, shift 
+        NetworkTable t = getTable();
+        Hashtable<String,Double> data = new Hashtable<String,Double>();
+        // Find the center of the two retroflective pieces of tape that are facing
+        // twoards each other!
+        Hashtable<String,Double>[] values = new Hashtable<String,Double>[]{createData(t,0),createData(t,1),createData(t,2)};
+        Arrays.sort(values, dataCompare);
+        double[] centerPos = center(values);
+        double distance = meterArea / values[1].get("a"); // Not sure wether this is the correct math
+        double shift = distance * meterDegreeLength * centerPos[0];
+
+        data.put("centerX",centerPos[0]);
+        data.put("centerY", centerPos[1]);
+        data.put("distance", distance);
+        data.put("shift", shift);
+        return data;
+    }
+
     @Override
     protected void initDefaultCommand() {
         // LITTERALLY DIE
