0309eb6da0cf4c412b94d4d7ab547b892bbd77e9
synecx <sirpythagoras@gmail.com>
2019-01-11 16:19:06 -0500

    Fix Talon values

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -7,11 +7,11 @@ public class PortMap {
     public static final int LEFT_JOYSTICK_BUTTON = 3;
     public static final int RIGHT_JOYSTICK_BUTTON = 4;
 
-    public static final int DRIVE_BACK_LEFT_TALON = 8; 
-    public static final int DRIVE_BACK_MIDDLE_TALON = 2; 
-    public static final int DRIVE_BACK_RIGHT_TALON = 4;
+    public static final int LEFT_FRONT_TALON = 8; 
+    public static final int LEFT_MIDDLE_TALON = 2; 
+    public static final int LEFT_BACK_TALON = 4;
 
-    public static final int DRIVE_FRONT_LEFT_TALON = 6;
-    public static final int DRIVE_FRONT_MIDDLE_TALON = 16;
-    public static final int DRIVE_FRONT_RIGHT_TALON = 15;
+    public static final int RIGHT_FRONT_TALON = 6;
+    public static final int RIGHT_MIDDLE_TALON = 16;
+    public static final int RIGHT_BACK_TALON = 15;
 }
