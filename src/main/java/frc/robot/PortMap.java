6ef76a870487ea9713d45d6979921f6064ffb12c
synecx <sirpythagoras@gmail.com>
2019-01-11 15:39:55 -0500

    Add Talons to PortMap

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -6,4 +6,12 @@ public class PortMap {
 
     public static final int LEFT_JOYSTICK_BUTTON = 3;
     public static final int RIGHT_JOYSTICK_BUTTON = 4;
+
+    public static final int DRIVE_BACK_LEFT_TALON = 8; 
+    public static final int DRIVE_BACK_MIDDLE_TALON = 2; 
+    public static final int DRIVE_BACK_RIGHT_TALON = 4;
+
+    public static final int DRIVE_FRONT_LEFT_TALON = 6;
+    public static final int DRIVE_FRONT_MIDDLE_TALON = 16;
+    public static final int DRIVE_FRONT_RIGHT_TALON = 15;
 }
