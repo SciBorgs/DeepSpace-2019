6a415b35d02feb494c874a42a744e5d7711e85d3
P250-SandDune <lib@bxscience.edu>
2019-02-12 17:12:40 -0500

    Lift

--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -3,21 +3,47 @@ import frc.robot.*;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class LiftSubsystem extends Subsystem {
-    public final double LIFT_P_GAIN = 0.1;
-    public final double LIFT_I_GAIN = 0.0;
-    public final double LIFT_D_GAIN = 0.0;
+    PID liftPID;
+    double liftP = 0.06;
+    double liftI = 0;
+    double liftD = 0.0065;
+    double encoderAtTop = 0;
+    double encoderAtLevelOne = 0;
+    double encoderAtLevelTwo = 0;
+    double encoderAtLevelThree = 0;
+    CANSparkMax liftSpark;
+    CANEncoder liftEncoder;
+    
+    public void goToZero() {
+        liftSpark = new CANSparkMax(PortMap.LIFT_SPARK, MotorType.kBrushless);
+        liftPID = new PID(liftP, liftI, liftD);
+        liftEncoder = liftSpark.getEncoder();
+    }
 
-    // TOOD: Maybe change the motor? xD
+    public void goToPositionOne(){
+        liftPID.add_measurement(encoderAtLevelOne - getPos());
+        liftSpark.set(liftPID.getLimitOutput(1));
+    }
+    
+    public void goToPositionTwo(){
+        liftPID.add_measurement(encoderAtLevelTwo - getPos());
+        liftSpark.set(liftPID.getLimitOutput(1));
+    }
+
+    public void goToPositionThree(){
+        liftPID.add_measurement(encoderAtLevelThree - getPos());
+        liftSpark.set(liftPID.getLimitOutput(1));
+    }
+
     public double getPos() {
-        //return Robot.driveSubsystem.rb.getEncoder().getPosition();
-        return 0;
+        System.out.println(liftEncoder.getPosition());
+        return liftEncoder.getPosition();
     }
-
-    public void setSpeed(double speed) {
-        //Robot.driveSubsystem.rb.set(speed);
-    }
-
 
     @Override
     protected void initDefaultCommand() {}
