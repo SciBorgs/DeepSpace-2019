3ce9ef8d9ceca311085edc8f34a59f00710fabe1
EllangoK <ellangok@bxscience.edu>
2019-02-18 13:53:45 -0500

    major updates getting ready for testing

--- src/main/java/frc/robot/commands/ZLiftCommand.java
+++ src/main/java/frc/robot/commands/ZLiftCommand.java
@@ -2,18 +2,20 @@ package frc.robot.commands;
 
 import frc.robot.*;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class ZLiftCommand extends Command {
-    private JoystickButton control;
+    private Joystick zLiftControl;
+    private Joystick speedControl;
 
-    public ZLiftCommand(JoystickButton control) {
+    public ZLiftCommand() {
         requires(Robot.zLiftSubsystem);
 
         // m_stop tells ZLiftCommand whether to stop.
         // m_stop is true when the button used for calling
         // ZLiftCommand is released.
-        this.control = control;
+        zLiftControl = Robot.oi.rightStick;
+        speedControl = Robot.oi.leftStick;
     }
 
     @Override
@@ -24,17 +26,19 @@ public class ZLiftCommand extends Command {
 
     @Override
     protected void execute() {
-        Robot.zLiftSubsystem.lift();
+        Robot.zLiftSubsystem.lift(Robot.driveSubsystem.processStick(zLiftControl));
+        Robot.driveSubsystem.setSpeed(speedControl, speedControl);
     }
 
     @Override
     protected boolean isFinished() {
-        return !control.get();
+        return false;
     }
 
     @Override
     protected void end() {
         Robot.zLiftSubsystem.reset();
+        Robot.driveSubsystem.setSpeedTank(0,0);;
     }
 
     @Override
