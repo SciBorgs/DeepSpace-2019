319b43eedbddd5aa958903093d8fcae58e0f5138
zevbo <lib@bxscience.edu>
2019-03-27 17:55:45 -0400

    implement getLastRow

--- src/main/java/frc/robot/logging/CSVHelper.java
+++ src/main/java/frc/robot/logging/CSVHelper.java
@@ -8,26 +8,68 @@ import java.io.*;
 public class CSVHelper {
 
     private String fileName;
+    private String fileContent;
     private BufferedReader reader;
     private PrintWriter writer;
-    private StringTokenizer line;
+    private ArrayList<String> topics;
+    private String lastLine;
 
     // name of the column = string at the top of the column
 
     public CSVHelper(String filename) throws IOException{
+        topics = new ArrayList<String>();
         fileName = filename;
         reader = new BufferedReader(new FileReader(fileName));
         writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+
+        StringTokenizer firstRow = new StringTokenizer(reader.readLine());
+        while(firstRow.hasMoreTokens()){
+            topics.add(firstRow.nextToken());
+        }
+
+        String tempLastLine = "";
+        String current = "";
+        while((current = reader.readLine()) != null){
+            fileContent += current;
+            tempLastLine = current;
+        }
+        lastLine = tempLastLine;
+
     }
 
-    public static void addRow(Hashtable<String,Object> row){
+    private void updateLastLine() throws IOException{
+        String tempLastLine = "";
+        String current = "";
+        while((current = reader.readLine()) != null){
+            fileContent += current;
+            tempLastLine = current;
+        }
+        lastLine = tempLastLine;
+    }
+
+    public Hashtable<String, String> getLastRow() {
+        try{
+            updateLastLine();
+        }catch (IOException e){
+            System.out.println("FILE NOT FOUND");
+        }
+        StringTokenizer lastLineString = new StringTokenizer(lastLine);
+        Hashtable<String, String> lastrow = new Hashtable<String, String>();
+        for(int i = 0;i < topics.size(); i++){
+            lastrow.put(topics.get(i), lastLineString.nextToken());
+        }
+
+        return lastrow;
+    }
+
+    public void addRow(Hashtable<String,Object> row){
         // Should add a row to the next empty row of the sheet
         // The key of the hashtable should be the name of the column
         
         return;
     }
 
-    public static void addColumn(String columnName){
+    public void addColumn(String columnName){
         // Adds a rightmost column with every cell empty except the top one w/ the column name
         return;
     }
