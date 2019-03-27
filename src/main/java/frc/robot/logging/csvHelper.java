ae42bda836a869ca05d0e34a88499c11dd324cf4
P250-SandDune <lib@bxscience.edu>
2019-03-27 15:21:48 -0400

    logging folder added

---
+++ src/main/java/frc/robot/logging/csvHelper.java
@@ -0,0 +1,30 @@
+package frc.robot.logging;
+
+import java.util.Hashtable;
+import java.util.ArrayList;
+
+public class csvHelper {
+
+    public static final String logigngDirPath = "/c/Users/SciBorgs/Desktop/Test Folder/DeepSpace-2019/src/main/java/frc/robot/logging/";
+    // Above is the path to the logging director
+    public static final String mainLoggingFile = "data.csv";
+
+    // name of the column = string at the top of the column
+
+    public static void addRow(Hashtable<String,Object> row){
+        // Should add a row to the next empty row of the sheet
+        // The key of the hashtable should be the name of the column
+        return;
+    }
+
+    public static void addColumn(Hashtable<String,Object> columnName){
+        // Adds a rightmost column with every cell empty except the top one w/ the column name
+        return;
+    }
+
+    public static ArrayList<Object> getColumns(){
+        // Gives a list of the column names in order
+        return new ArrayList<Object>();
+    }
+
+}
