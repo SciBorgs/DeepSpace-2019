eaba02d079273da738ccfa4e3602d3575eca185e
P250-SandDune <lib@bxscience.edu>
2019-04-01 17:14:46 -0400

    cargo follow updated, template for the rest of the commands

--- src/main/java/frc/robot/commands/CargoFollowCommand.java
+++ src/main/java/frc/robot/commands/CargoFollowCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
+import frc.robot.logging.Logger.CommandStatus;
 import frc.robot.logging.Logger.DefaultValue;
 
 public class CargoFollowCommand extends CommandGroup {
@@ -14,14 +15,14 @@ public class CargoFollowCommand extends CommandGroup {
     }
 
     @Override protected void initialize() {
-		Robot.logger.addData(this.fileName, Robot.logger.commandStatus, "initializing", DefaultValue.Empty);
+		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Initializing);
 		System.out.println("following cargo");
 		Robot.cargoFollowing.modeToCargo();
         Robot.gearShiftSubsystem.shiftUp();
     }
 
 	@Override protected void execute(){
-		Robot.logger.addData(this.fileName, Robot.logger.commandStatus, "executing", DefaultValue.Empty);
+		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Executing);
 		System.out.println("following cargo");
 		if (Robot.oi.cargoFollowButton.get()){
 			Robot.driveSubsystem.assistedDriveMode();
@@ -37,14 +38,14 @@ public class CargoFollowCommand extends CommandGroup {
 	}
 
 	@Override protected void end(){
-		Robot.logger.addData(this.fileName, Robot.logger.commandStatus, "ending", DefaultValue.Empty);
+		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Ending);
 		Robot.gearShiftSubsystem.shiftDown();
 		Robot.driveSubsystem.manualDriveMode();
 		//return;
 	}
 
 	@Override protected void interrupted(){
-		Robot.logger.addData(this.fileName, Robot.logger.commandStatus, "interrupted", DefaultValue.Empty);
+		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Interrupted);
 		end();
 	}
 }
