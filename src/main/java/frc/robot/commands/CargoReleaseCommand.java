0cdd4060e042ecdd1f8eaf7f14a88cf767745eb6
zevbo <lib@bxscience.edu>
2019-04-01 16:48:15 -0400

    added logging functionality to commands from cargofollowup to resetlineupinfo

--- src/main/java/frc/robot/commands/CargoReleaseCommand.java
+++ src/main/java/frc/robot/commands/CargoReleaseCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
+import frc.robot.logging.Logger.DefaultValue;
 
 public class CargoReleaseCommand extends InstantCommand {
 
@@ -12,6 +13,7 @@ public class CargoReleaseCommand extends InstantCommand {
     }
 
     @Override protected void execute() {
+        Robot.logger.addData(this.fileName, Robot.logger.commandStatus, "executing", DefaultValue.Empty);
         System.out.println("releasing cargo");
         Robot.intakeSubsystem.openArm();
         Robot.intakeSubsystem.spit();
