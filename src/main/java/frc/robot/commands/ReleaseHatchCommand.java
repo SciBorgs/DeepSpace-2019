6428ca36cbb542dd3bf4470ee5800ba482b313b2
Zev Minsky-Primus <goobjar@tiamat.lan>
2019-05-11 14:08:21 -0400

    intakeSubsystem clean-up

--- src/main/java/frc/robot/commands/ReleaseHatchCommand.java
+++ src/main/java/frc/robot/commands/ReleaseHatchCommand.java
@@ -18,7 +18,6 @@ public class ReleaseHatchCommand extends InstantCommand {
         System.out.println("Release Hatch command");
         Robot.intakeSubsystem.releaseHatch();
         Robot.intakeSubsystem.closeArm();
-        Robot.intakeSubsystem.updateHoldingHatch(false);
         Robot.driveSubsystem.setTankMultiplier(RELEASE_HATCH_DRIVE_MULTIPLIER);
         //Robot.gearShiftSubsystem.shiftUp();
     }
