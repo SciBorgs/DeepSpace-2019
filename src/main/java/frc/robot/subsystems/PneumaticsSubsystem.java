9d06c5c4228339bdaeec30d38fcde874c30ff5ab
Zev Minsky-Primus <goobjar@tiamat.lan>
2019-05-12 12:57:37 -0400

    pneumaticsSubystem clean-up

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -7,17 +7,15 @@ import frc.robot.PortMap;
 import frc.robot.Robot;
 import frc.robot.logging.Logger.DefaultValue;
 
-// FILE HAS NOT BEEN CLEANED UP //
 public class PneumaticsSubsystem extends Subsystem {
+  
   private AnalogInput pressureSensor;
   private final double NORMALIZED_SUPPLY_VOLTAGE = 5.0;
   private Compressor compressor;
-	private final String fileName = "PneumaticsSubsystem.java";
+  private final String fileName = "PneumaticsSubsystem.java";
+  
   @Override
-  public void initDefaultCommand() {
-   
-  }
-
+  public void initDefaultCommand() {}
 
   public PneumaticsSubsystem() {
     this.pressureSensor = new AnalogInput(PortMap.PRESSURE_SENSOR);
@@ -29,18 +27,11 @@ public class PneumaticsSubsystem extends Subsystem {
 	}
 
   public double getPressure() {
-    return 250.0 * pressureSensor.getVoltage() / NORMALIZED_SUPPLY_VOLTAGE - 15.0;
+    // QUESTION: What's up with 250 and 15? Definitely at minimum bordering on magic numbers
+    return 250.0 * this.pressureSensor.getVoltage() / NORMALIZED_SUPPLY_VOLTAGE - 15.0;
   }
 
-  public void startCompressor(){
-    compressor.start();
-  }
-
-  public void stopCompressor(){
-    compressor.stop();
-  }
-
-  public double getRawVoltage() {
-    return pressureSensor.getVoltage();
-  }
+  public void startCompressor(){this.compressor.start();}
+  public void stopCompressor() {this.compressor.stop();}
+  public double getRawVoltage(){return pressureSensor.getVoltage();}
 }
