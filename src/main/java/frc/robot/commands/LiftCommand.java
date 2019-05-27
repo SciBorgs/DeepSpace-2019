821a844871c181939dafccc78bfabd6b7c144de7
Admin <tobiasalam@gmail.com>
2019-05-27 19:11:09 -0400

    got rid of unnecessary comments + imports

--- src/main/java/frc/robot/commands/LiftCommand.java
+++ src/main/java/frc/robot/commands/LiftCommand.java
@@ -1,16 +1,9 @@
 package frc.robot.commands;
 
 import frc.robot.Robot;
-import frc.robot.subsystems.LiftSubsystem.Target;
-
-import edu.wpi.first.wpilibj.command.ConditionalCommand;
-import edu.wpi.first.wpilibj.command.InstantCommand;
-
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.logging.Logger.CommandStatus;
-import frc.robot.logging.Logger.DefaultValue;
 
 public class LiftCommand extends Command {
 
@@ -27,22 +20,14 @@ public class LiftCommand extends Command {
 		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Initializing);
 		Robot.liftSubsystem.autoArmMode();
 		Robot.liftSubsystem.currentlyTiltingArm();
-		// Robot.liftSubsystem.autoCascadeMode(); // Comment for regular auto-carriage
 	}
 
 	@Override protected void execute(){
 		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Executing);
-		//Robot.liftSubsystem.moveToTarget(Robot.liftSubsystem.getTarget()); // Comment for regular auto-carriage
 		Robot.liftSubsystem.moveArmToTarget(Robot.liftSubsystem.getTarget()); // Uncomment for regular auto-carriage
 	}
 
 	@Override protected boolean isFinished(){
-		/*if (lastStatic){
-			return Robot.liftSubsystem.isArmStatic();
-		} else {
-			lastStatic = Robot.liftSubsystem.isArmStatic();
-			return false;
-		}*/
 		return false;
 	}
 
