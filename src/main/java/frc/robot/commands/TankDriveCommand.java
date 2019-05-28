20f94f9be806b3fe7d2439393cf57eb2d3a34a35
swanand <kaneres@bxscience.edu>
2019-05-28 10:05:26 -0400

    fixed some stuff
    - removed unused imports
    - removed unnecessary print statements (they were probably for testing)
    
    not sure what you mean by 'left and right stick fields'

--- src/main/java/frc/robot/commands/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -1,16 +1,8 @@
 package frc.robot.commands;
 
 import frc.robot.Robot;
-import frc.robot.Utils;
-import frc.robot.subsystems.LiftSubsystem.Target;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.logging.Logger.CommandStatus;
 import frc.robot.logging.Logger.DefaultValue;
@@ -21,16 +13,12 @@ public class TankDriveCommand extends InstantCommand {
     private Joystick rightStick, leftStick;
     
     public TankDriveCommand() {
-        this.rightStick = Robot.oi.rightStick;
-        this.leftStick  = Robot.oi.leftStick;
+        rightStick = Robot.oi.rightStick;
+        leftStick  = Robot.oi.leftStick;
     }
 
     @Override protected void execute() {
-		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Executing);
+	Robot.logger.logCommandStatus(this.fileName, CommandStatus.Executing);
         Robot.driveSubsystem.setSpeed(leftStick, rightStick);
-        
-        System.out.println("left stick current: " + leftStick);
-        System.out.println("right stick current: " + rightStick);
-    }
-        
+    }   
 }
