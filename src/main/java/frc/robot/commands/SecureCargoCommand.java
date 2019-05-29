2a59f0075f70fce88013956d7f694ffca5e36679
Darmin-Geek <34427428+Darmin-Geek@users.noreply.github.com>
2019-05-29 16:47:39 -0400

    Update SecureCargoCommand.java

--- src/main/java/frc/robot/commands/SecureCargoCommand.java
+++ src/main/java/frc/robot/commands/SecureCargoCommand.java
@@ -3,16 +3,15 @@ package frc.robot.commands;
 import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.logging.Logger.CommandStatus;
-import frc.robot.logging.Logger.DefaultValue;
 
 
 public class SecureCargoCommand extends InstantCommand {
-    private final String fileName = "SecureCargoCommand.java";
+    private final String FILENAME = "SecureCargoCommand.java";
 
     public SecureCargoCommand(){}
 
     @Override protected void execute(){
-		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Executing);
+		Robot.logger.logCommandStatus(this.FILENAME, CommandStatus.Executing);
         Robot.intakeSubsystem.secureCargo();
     }
 }
