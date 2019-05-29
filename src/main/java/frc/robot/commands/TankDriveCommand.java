7278f7074f6de22a1a5d72cf563cab0a641ec8b5
swanand <kaneres@bxscience.edu>
2019-05-29 19:24:10 -0400

    I think this works
    - got rid of initializing joysticks completely
    - removed unused imports

--- src/main/java/frc/robot/commands/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -1,25 +1,16 @@
 package frc.robot.commands;
 
 import frc.robot.Robot;
-
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.logging.Logger.CommandStatus;
-import frc.robot.logging.Logger.DefaultValue;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 public class TankDriveCommand extends InstantCommand {
     private final String FILENAME = "TankDriveCommand.java";
-    private Joystick rightStick, leftStick;
-    
-    public TankDriveCommand() {
-        this.rightStick = Robot.oi.rightStick;
-        this.leftStick  = Robot.oi.leftStick;
-    }
 
     @Override 
     protected void execute() {
 	Robot.logger.logCommandStatus(FILENAME, CommandStatus.Executing);
-        Robot.driveSubsystem.setSpeed(leftStick, rightStick);
+        Robot.driveSubsystem.setSpeed(Robot.oi.leftStick, Robot.oi.rightStick);
     }   
 }
