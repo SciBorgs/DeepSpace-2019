8942c66a983bdd43b940d717300a129a23e14e65
Tobias Alam <tobiasalam@gmail.com>
2019-05-29 16:24:41 -0400

    fixed field naming conventions

--- src/main/java/frc/robot/commands/LiftCommand.java
+++ src/main/java/frc/robot/commands/LiftCommand.java
@@ -7,7 +7,7 @@ import frc.robot.logging.Logger.CommandStatus;
 
 public class LiftCommand extends Command {
 
-	private final String fileName = "LiftCommand.java";
+	private final String FILE_NAME = "LiftCommand.java";
 
 	private boolean lastStatic;
 
@@ -17,26 +17,26 @@ public class LiftCommand extends Command {
 	}
 
 	@Override protected void initialize(){
-		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Initializing);
+		Robot.logger.logCommandStatus(FILE_NAME, CommandStatus.Initializing);
 		Robot.liftSubsystem.autoArmMode();
 		Robot.liftSubsystem.currentlyTiltingArm();
 	}
 
 	@Override protected void execute(){
-		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Executing);
+		Robot.logger.logCommandStatus(FILE_NAME, CommandStatus.Executing);
 		Robot.liftSubsystem.moveArmToTarget(Robot.liftSubsystem.getTarget()); // Uncomment for regular auto-carriage
 	}
 
 	@Override protected boolean isFinished(){ return false; }
 
 	@Override protected void end(){
-		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Ending);
+		Robot.logger.logCommandStatus(FILE_NAME, CommandStatus.Ending);
 		Robot.liftSubsystem.manualArmMode();
 		Robot.liftSubsystem.manualCascadeMode();
 	}
 
 	@Override protected void interrupted(){
-		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Interrupted);
+		Robot.logger.logCommandStatus(FILE_NAME, CommandStatus.Interrupted);
 		end();
 		return;
 	}
