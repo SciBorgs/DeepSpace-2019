eacad100a9b63ba6147ef6dcbb5224eb9006450e
Zawad Munshi <32941432+zawadm321@users.noreply.github.com>
2019-05-30 11:56:41 -0400

    Fixed names

--- src/main/java/frc/robot/commands/ReleaseHatchCommand.java
+++ src/main/java/frc/robot/commands/ReleaseHatchCommand.java
@@ -6,13 +6,13 @@ import frc.robot.logging.Logger.CommandStatus;
 
 public class ReleaseHatchCommand extends InstantCommand {
     private final String fileName = "ReleaseHatchCommand.java";
-    private double RELEASE_HATCH_DRIVE_MULTIPLIER = 0.5;
+    private double ReleaseHatchDriveMultiplier = 0.5;
 
     @Override protected void execute(){
 		Robot.logger.logCommandStatus(this.fileName, CommandStatus.Executing);
         Robot.intakeSubsystem.releaseHatch();
         Robot.intakeSubsystem.closeArm();
-        Robot.driveSubsystem.setDriveMultiplier(this.RELEASE_HATCH_DRIVE_MULTIPLIER);           
+        Robot.driveSubsystem.setDriveMultiplier(this.ReleaseHatchDriveMultiplier);           
 
     }
 }
