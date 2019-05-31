70fd7e955e8786c859904b2f1103c40689c78bb6
Zawad Munshi <32941432+zawadm321@users.noreply.github.com>
2019-05-30 21:13:54 -0400

    Changed name of Filename variable

--- src/main/java/frc/robot/commands/ReleaseHatchCommand.java
+++ src/main/java/frc/robot/commands/ReleaseHatchCommand.java
@@ -5,11 +5,11 @@ import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.logging.Logger.CommandStatus;
 
 public class ReleaseHatchCommand extends InstantCommand {
-    private final String fileName = "ReleaseHatchCommand.java";
+    private final String FILENAME = "ReleaseHatchCommand.java";
     private final double RELEASE_HATCH_DRIVE_MULTIPLIER = 0.5;
 
     @Override protected void execute(){
-		Robot.logger.logCommandStatus(fileName, CommandStatus.Executing);
+		Robot.logger.logCommandStatus(FILENAME, CommandStatus.Executing);
         Robot.intakeSubsystem.releaseHatch();
         Robot.intakeSubsystem.closeArm();
         Robot.driveSubsystem.setDriveMultiplier(RELEASE_HATCH_DRIVE_MULTIPLIER);           
