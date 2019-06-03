df08324062aebbec77749ec229818cd5ecf11ba9
Darmin-Geek <34427428+Darmin-Geek@users.noreply.github.com>
2019-06-03 18:29:31 -0400

    Update SwerveTankDriveCommand.java
    deleted fields
    added variable for turn amount

--- src/main/java/frc/robot/commands/SwerveTankDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveTankDriveCommand.java
@@ -2,23 +2,21 @@ package frc.robot.commands;
 
 import frc.robot.Robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.logging.Logger.CommandStatus;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 public class SwerveTankDriveCommand extends InstantCommand {
     private final String FILENAME = "SwerveTankDriveCommand.java";
-    private Joystick leftStick = Robot.oi.leftStick;
-    private Joystick rightStick = Robot.oi.rightStick;
-
+   
     
     public SwerveTankDriveCommand(){}
 
     @Override protected void execute() {
         Robot.logger.logCommandStatus(FILENAME, CommandStatus.Executing);
 
-        double forward = Robot.driveSubsystem.processStick(this.leftStick);
-        Robot.driveSubsystem.setSpeedTankForwardTurningPercentage(forward, this.rightStick.getX());
+        double forward = Robot.driveSubsystem.processStick(Robot.oi.leftStick);
+        double turnAmount = Robot.oi.rightStick.getX();
+        Robot.driveSubsystem.setSpeedTankForwardTurningPercentage(forward, turnAmount);
 
         
     }
