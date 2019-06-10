8798efe556b2fdfa6bae6abaf1f6ad109b918d86
Dhinak G <dhinak2004@gmail.com>
2019-06-10 15:12:17 -0400

    Spacing

--- src/main/java/frc/robot/commands/CargoFollowCommand.java
+++ src/main/java/frc/robot/commands/CargoFollowCommand.java
@@ -5,42 +5,42 @@ import frc.robot.Robot;
 import frc.robot.logging.Logger.CommandStatus;
 
 public class CargoFollowCommand extends CommandGroup {
-
+    
     private static final String FILENAME = "CargoFollowCommand.java";
-
+    
     public CargoFollowCommand() {
         requires(Robot.driveSubsystem);
         addParallel(new SuckCommand());
     }
-
+    
     @Override
     protected void initialize() {
-	Robot.logger.logCommandStatus(FILENAME, CommandStatus.Initializing);
-	Robot.following.resetCargoPID();
+        Robot.logger.logCommandStatus(FILENAME, CommandStatus.Initializing);
+        Robot.following.resetCargoPID();
     }
 	
     @Override
     protected void execute(){
-	Robot.following.modeToCargo();
-	Robot.logger.logCommandStatus(FILENAME, CommandStatus.Executing);
-	if (Robot.oi.cargoFollowButton.get()){
-		Robot.driveSubsystem.assistedDriveMode();
-		Robot.following.followBall();
-	} else { Robot.driveSubsystem.manualDriveMode(); }
+        Robot.following.modeToCargo();
+        Robot.logger.logCommandStatus(FILENAME, CommandStatus.Executing);
+        if (Robot.oi.cargoFollowButton.get()){
+            Robot.driveSubsystem.assistedDriveMode();
+            Robot.following.followBall();
+        } else { Robot.driveSubsystem.manualDriveMode(); }
     }
-
+    
     @Override
     protected boolean isFinished() { return !Robot.oi.suckButton.get(); }
-
+    
     @Override
     protected void end(){
-	Robot.logger.logCommandStatus(FILENAME, CommandStatus.Ending);
-	Robot.driveSubsystem.manualDriveMode();
+        Robot.logger.logCommandStatus(FILENAME, CommandStatus.Ending);
+        Robot.driveSubsystem.manualDriveMode();
     }
-
+    
     @Override
     protected void interrupted(){
-	Robot.logger.logCommandStatus(FILENAME, CommandStatus.Interrupted);
-	end();
+        Robot.logger.logCommandStatus(FILENAME, CommandStatus.Interrupted);
+        end();
     }
 }
