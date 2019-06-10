02bc99f2842024b99c9803347e2bb44bc667b2e5
Tobias Alam <tobiasalam@gmail.com>
2019-06-10 15:40:43 -0400

    removed unnecessary prints and comments

--- src/main/java/frc/robot/commands/PopHatchCommand.java
+++ src/main/java/frc/robot/commands/PopHatchCommand.java
@@ -1,9 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package frc.robot.commands;
 
@@ -12,24 +6,18 @@ import frc.robot.Robot;
 
 public class PopHatchCommand extends TimedCommand {
   public static final double TIMEOUT = .2;
-  public PopHatchCommand() {
-    super("pop hatch", TIMEOUT);
-  }
+  public PopHatchCommand() { super("pop hatch", TIMEOUT);}
 
   @Override
   protected void initialize() {
-    System.out.println("popping");
     Robot.intakeSubsystem.extendPopHatchPistons();
   }
 
   @Override
   protected void end() {
-    System.out.println("retracting")
     Robot.intakeSubsystem.retractPopHatchPistons();
   }
 
   @Override
-  protected void interrupted() {
-    end();
-  }
+  protected void interrupted() { end(); }
 }
